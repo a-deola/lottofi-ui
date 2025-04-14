f7188c1b6e135be623123535ceb037e177927a60
Adeola Samuel <meetprecious1403@gmail.com>
2025-04-14 17:15:31 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+        <h1 className="text-4xl font-bold text-center">
+          Hello
+        </h1>
+    </div>
+  );
+}
